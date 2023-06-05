5609864eea5ebf3bff63f4534df1a622ea6ec909
Kyeongeun Park <kellypark2032@gmail.com>
2023-06-05 21:26:16 +0900

    first commit

--- prepare/front/components/AppLayout.tsx
+++ prepare/front/components/AppLayout.tsx
@@ -1,4 +1,6 @@
 import {ReactNode} from "react";
+import {Col, Input, Menu, Row} from "antd";
+import Link from "next/link";
 
 type LayoutProps = {
     children: ReactNode;
@@ -7,8 +9,33 @@ type LayoutProps = {
 const AppLayout = ({children}: LayoutProps) => {
     return (
         <div>
-            <div>공통메뉴</div>
-            <div>{children}</div>
+            <Menu mode="horizontal">
+                <Menu.Item>
+                    <Link href="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/profile">Profile</Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Input.Search enterButton style={{
+                        verticalAlign: 'middle'
+                    }}/>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/signup">SignUp</Link>
+                </Menu.Item>
+            </Menu>
+            <Row gutter={8}>
+                <Col xs={24} md={6}>왼쪽메뉴</Col>
+                <Col xs={24} md={12}>{children}</Col>
+                <Col xs={24} md={6}>
+                    <a href="https://github.com/KellyPark96"
+                       target="_blank"
+                       rel="noreferrer noopener">
+                        Github
+                    </a>
+                </Col>
+            </Row>
         </div>
     )
 }
